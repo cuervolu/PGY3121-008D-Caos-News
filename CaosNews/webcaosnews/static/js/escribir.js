30ae809390e25213d4667c633fa608f4076b128a
cuervolu <cuervolu@protonmail.com>
2022-05-15 16:35:43 -0400

    intento de alerta

--- CaosNews/webcaosnews/static/js/escribir.js
+++ CaosNews/webcaosnews/static/js/escribir.js
@@ -1,27 +1,27 @@
-// Validación
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
+(() => {
     'use strict'
   
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-    
+    const forms = document.querySelectorAll('.needs-validation')
   
     // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+          Swal.fire(
+            'Good job!',
+            'You clicked the button!',
+            'success'
+          )
+        }
   
-          form.classList.add('was-validated')
-        }, false)
-      })
+        form.classList.add('was-validated')
+      }, false)
+    })
   })()
-
-
 
 const useDarkMode = window.matchMedia('(prefers-color-scheme: white)').matches;
 const isSmallScreen = window.matchMedia('(max-width: 1023.5px)').matches;
