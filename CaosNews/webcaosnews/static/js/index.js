786a9d894fd65c35e4e44b1e52e16bab821aac0b
Cuervolu <cuervolu@protonmail.com>
2022-07-13 16:35:24 -0400

    Corrección final hecha en clases

--- CaosNews/webcaosnews/static/js/index.js
+++ CaosNews/webcaosnews/static/js/index.js
@@ -4,7 +4,7 @@ const API_URL2 = 'https://api.gael.cloud/general/public/monedas/UF';
 fetch(API_URL1).then(function (response) {
     return response.json();
 }).then(function (monedas) {
-    moneda = Math.round(parseInt(monedas.Valor))
+    moneda = (monedas.Valor).toLocaleString('es-CL', { minimumFractionDigits: 2, style: 'currency', currency: 'CLP' });
     document.getElementById("DolarO").innerHTML = 'El valor actual del Dólar es $' + moneda;
 }).catch(function (error) {
     console.log('Requestfailed', error);
@@ -13,7 +13,7 @@ fetch(API_URL1).then(function (response) {
 fetch(API_URL2).then(function (response) {
     return response.json();
 }).then(function (monedas) {
-    moneda = Math.round(parseInt(monedas.Valor))
+    moneda = (monedas.Valor).toLocaleString('es-CL', { minimumFractionDigits: 2, style: 'currency', currency: 'CLP' });
     document.getElementById("UTF").innerHTML = 'El valor actual del UF es $' + moneda;
 }).catch(function (error) {
     console.log('Requestfailed', error);
--- CaosNews/webcaosnews/views.py
+++ CaosNews/webcaosnews/views.py
@@ -13,14 +13,15 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from django.http import Http404
 from django.db.models import Q
-#importar libreria de conexión HTTP
+# importar libreria de conexión HTTP
 import requests
 from rest_framework import viewsets
-#JsonResponse
+# JsonResponse
 from django.http import JsonResponse
 # Create your views here
 usu = ''
 cantidad = 0
+
 
 def cantidad_no_publicados(usuario):
     cantidad = Noticias.objects.filter(
@@ -30,32 +31,38 @@ def cantidad_no_publicados(usuario):
 
 def index(request):
     noticias = Noticias.objects.filter(aprobada=True).order_by('-fecha')[:3]
-    noticiasN = Noticias.objects.filter(categoria__nombre = 'Nacional' ,aprobada=True).order_by('-fecha')
-    notDeporte = Noticias.objects.filter(categoria__nombre = 'Deportes' ,aprobada=True).last()
-    contexto = {"noticiasN": noticiasN, "noticias": noticias, "notDeporte": notDeporte}
-    return render(request, "index.html",contexto)
+    noticiasN = Noticias.objects.filter(
+        categoria__nombre='Nacional', aprobada=True).order_by('-fecha')
+    notDeporte = Noticias.objects.filter(
+        categoria__nombre='Deportes', aprobada=True).last()
+    contexto = {"noticiasN": noticiasN,
+                "noticias": noticias, "notDeporte": notDeporte}
+    return render(request, "index.html", contexto)
 
 
-def articulo(request,id):
+def articulo(request, id):
     noticia = Noticias.objects.get(id=id)
-    contexto = {"noticia":noticia}
-    return render(request, "articulo.html",contexto)
+    contexto = {"noticia": noticia}
+    return render(request, "articulo.html", contexto)
+
 
 def buscarArticulo(request):
     if request.method == 'POST':
         searched = request.POST['txtBuscar']
-        entity = Noticias.objects.filter( Q(titulo__icontains=searched) | Q(autor__icontains=searched)|Q(etiquetas__icontains=searched)) # buscar por titulo, etiquetas  y autor
-        return render(request, "buscarArticulo.html",{"buscado": searched,"entity": entity})
+        entity = Noticias.objects.filter(Q(titulo__icontains=searched) | Q(autor__icontains=searched) | Q(
+            etiquetas__icontains=searched))  # buscar por titulo, etiquetas  y autor
+        return render(request, "buscarArticulo.html", {"buscado": searched, "entity": entity})
     else:
         return render(request, "buscarArticulo.html")
     return render(request, "buscarArticulo.html")
 
+
 def galeria(request):
     noticias = Noticias.objects.filter(aprobada=True).order_by('-fecha')
     # contexto = {"noticias": noticias}
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     try:
-        paginator = Paginator(noticias,5)
+        paginator = Paginator(noticias, 5)
         noticias = paginator.page(page)
     except:
         raise Http404
@@ -83,11 +90,12 @@ def contacto(request):
 
 
 def deportes(request):
-    noticias = Noticias.objects.filter(aprobada=True,categoria__nombre = 'Deportes').order_by('-fecha')
+    noticias = Noticias.objects.filter(
+        aprobada=True, categoria__nombre='Deportes').order_by('-fecha')
     # contexto = {"noticias": noticias}
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     try:
-        paginator = Paginator(noticias,5)
+        paginator = Paginator(noticias, 5)
         noticias = paginator.page(page)
     except:
         raise Http404
@@ -95,7 +103,7 @@ def deportes(request):
         'entity': noticias,
         'paginator': paginator
     }
-    return render(request, "deportes.html",data)
+    return render(request, "deportes.html", data)
 
 
 def login(request):
@@ -122,25 +130,28 @@ def registro(request):
         formulario = CustomUserCreationForm(data=request.POST)
         if formulario.is_valid():
             formulario.save()
-            user = authenticate(username=formulario.cleaned_data['username'], password=formulario.cleaned_data['password1'])
+            user = authenticate(
+                username=formulario.cleaned_data['username'], password=formulario.cleaned_data['password1'])
             login_aut(request, user)
             messages.success(
                 request, 'Te has registrado correctamente.')
-            #redirigir al home
+            # redirigir al home
             return redirect('/')
         data['form'] = formulario
-    return render(request, "registration/registro.html",data)
+    return render(request, "registration/registro.html", data)
+
 
 def terms(request):
     return render(request, "terms.html")
 
 
 def politica(request):
-    noticias = Noticias.objects.filter(aprobada=True,categoria__nombre = 'Política').order_by('-fecha')
+    noticias = Noticias.objects.filter(
+        aprobada=True, categoria__nombre='Política').order_by('-fecha')
     # contexto = {"noticias": noticias}
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     try:
-        paginator = Paginator(noticias,5)
+        paginator = Paginator(noticias, 5)
         noticias = paginator.page(page)
     except:
         raise Http404
@@ -148,15 +159,16 @@ def politica(request):
         'entity': noticias,
         'paginator': paginator
     }
-    return render(request, "politica.html",data)
+    return render(request, "politica.html", data)
 
 
 def nacional(request):
-    noticias = Noticias.objects.filter(aprobada=True,categoria__nombre = 'Nacional').order_by('-fecha')
+    noticias = Noticias.objects.filter(
+        aprobada=True, categoria__nombre='Nacional').order_by('-fecha')
     # contexto = {"noticias": noticias}
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     try:
-        paginator = Paginator(noticias,5)
+        paginator = Paginator(noticias, 5)
         noticias = paginator.page(page)
     except:
         raise Http404
@@ -168,11 +180,12 @@ def nacional(request):
 
 
 def mundo(request):
-    noticias = Noticias.objects.filter(aprobada=True,categoria__nombre = 'Mundo').order_by('-fecha')
+    noticias = Noticias.objects.filter(
+        aprobada=True, categoria__nombre='Mundo').order_by('-fecha')
     # contexto = {"noticias": noticias}
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     try:
-        paginator = Paginator(noticias,5)
+        paginator = Paginator(noticias, 5)
         noticias = paginator.page(page)
     except:
         raise Http404
@@ -180,7 +193,7 @@ def mundo(request):
         'entity': noticias,
         'paginator': paginator
     }
-    return render(request, "mundo.html",data)
+    return render(request, "mundo.html", data)
 
 
 def cerrar_sesion(request):
@@ -188,29 +201,35 @@ def cerrar_sesion(request):
     return render(request, "index.html")
 
 
-
 @login_required(login_url='login')
-@permission_required('webcaosnews.view_noticias',login_url='login')
+@permission_required('webcaosnews.view_noticias', login_url='login')
 def panel(request):
     usu = request.user.username  # recuperar nombre de usuario
-    noticias = Noticias.objects.filter(usuario=usu, aprobada=False).order_by('-fecha')
+    noticias = Noticias.objects.filter(
+        usuario=usu, aprobada=False).order_by('-fecha')
     cantidadNoticias = Noticias.objects.filter(usuario=usu).count()
-    cantidadNoticiasAprobadas = Noticias.objects.filter(usuario=usu, aprobada=True).count()
+    cantidadNoticiasAprobadas = Noticias.objects.filter(
+        usuario=usu, aprobada=True).count()
     cantidad = cantidad_no_publicados(usu)
-    coeficiente = cantidadNoticiasAprobadas / cantidadNoticias
+    try:
+        coeficiente = cantidadNoticiasAprobadas / cantidadNoticias
+    except ZeroDivisionError as err:
+        coeficiente = 0
     porcentaje = round(coeficiente * 100)
-    contexto = {"noticias": noticias, "cantidad": cantidad, "cantidadNoticias": cantidadNoticias, "cantidadNoticiasAprobadas": cantidadNoticiasAprobadas, "porcentaje": porcentaje}
+    contexto = {"noticias": noticias, "cantidad": cantidad, "cantidadNoticias": cantidadNoticias,
+                "cantidadNoticiasAprobadas": cantidadNoticiasAprobadas, "porcentaje": porcentaje}
     return render(request, "panel/panel.html", contexto)
 
+
 @login_required(login_url='login')
-@permission_required('webcaosnews.change_noticias',login_url='login')
-@permission_required('webcaosnews.delete_noticias',login_url='login')
-@permission_required('webcaosnews.view_noticias',login_url='login')
+@permission_required('webcaosnews.change_noticias', login_url='login')
+@permission_required('webcaosnews.delete_noticias', login_url='login')
+@permission_required('webcaosnews.view_noticias', login_url='login')
 def listar(request):
-    noticias = Noticias.objects.filter(usuario= request.user).order_by('-fecha')
-    page = request.GET.get('page',1)
+    noticias = Noticias.objects.filter(usuario=request.user).order_by('-fecha')
+    page = request.GET.get('page', 1)
     try:
-        paginator = Paginator(noticias,5)
+        paginator = Paginator(noticias, 5)
         noticias = paginator.page(page)
     except:
         raise Http404
@@ -218,20 +237,31 @@ def listar(request):
         'entity': noticias,
         'paginator': paginator
     }
-    return render(request, "panel/listado.html",data)
+    return render(request, "panel/listado.html", data)
+
 
 @login_required(login_url='login')
-@permission_required('webcaosnews.change_noticias',login_url='login')
-@permission_required('webcaosnews.delete_noticias',login_url='login')
-@permission_required('webcaosnews.view_noticias',login_url='login')
+@permission_required('webcaosnews.change_noticias', login_url='login')
+@permission_required('webcaosnews.delete_noticias', login_url='login')
+@permission_required('webcaosnews.view_noticias', login_url='login')
 def modificarNoticia(request, id):
-    Noticia = get_object_or_404(Noticias, id = id)
+    Noticia = get_object_or_404(Noticias, id=id)
     data = {
-        'form' : EscribirForm(instance=Noticia),
+        'form': EscribirForm(instance=Noticia),
     }
     if request.method == 'POST':
-        formulario = EscribirForm(data=request.POST, files=request.FILES, instance=Noticia)
+        formulario = EscribirForm(
+            data=request.POST, files=request.FILES, instance=Noticia)
+        imagenNoticia = formulario.files.getlist('ImagenNoticia')
         if formulario.is_valid():
+            instance = formulario.save(commit=False)
+            instance.save()
+            for i in imagenNoticia:
+                img = ImagenNoticia(imagen=i, noticia=instance)
+                img.save()
+                # instance.imagenes.add(img)
+            # instance.save()
+            print(f'Imagenes: {imagenNoticia}')
             formulario.save()
             messages.success(
                 request, 'Tu artículo ha sido modificado', extra_tags='alerta')
@@ -241,35 +271,37 @@ def modificarNoticia(request, id):
             messages.error(
                 request, 'Ha habido un error.', extra_tags='fallo')
             data['form'] = formulario
-    return render(request, "panel/modificarNoticia.html",data)
+    return render(request, "panel/modificarNoticia.html", data)
+
 
 @login_required(login_url='login')
-@permission_required('webcaosnews.change_noticias',login_url='login')
-@permission_required('webcaosnews.delete_noticias',login_url='login')
-@permission_required('webcaosnews.view_noticias',login_url='login')
+@permission_required('webcaosnews.change_noticias', login_url='login')
+@permission_required('webcaosnews.delete_noticias', login_url='login')
+@permission_required('webcaosnews.view_noticias', login_url='login')
 def eliminarNoticia(request, id):
-     Noticia = get_object_or_404(Noticias, id = id)
-     Noticia.delete()
-     messages.success(
-         request, 'Tu artículo ha sido eliminado', extra_tags='alerta')
-     return redirect(to='list')
- 
+    Noticia = get_object_or_404(Noticias, id=id)
+    Noticia.delete()
+    messages.success(
+        request, 'Tu artículo ha sido eliminado', extra_tags='alerta')
+    return redirect(to='list')
+
+
 @login_required(login_url='login')
-@permission_required('webcaosnews.add_noticias',login_url='login')
+@permission_required('webcaosnews.add_noticias', login_url='login')
 def escribir(request):
     data = {
-        'form': EscribirForm(initial={'usuario':request.user.username}),
+        'form': EscribirForm(initial={'usuario': request.user.username}),
     }
     if request.method == 'POST':
         formulario = EscribirForm(data=request.POST, files=request.FILES)
         imagenNoticia = formulario.files.getlist('ImagenNoticia')
         if formulario.is_valid():
-            instance=formulario.save(commit=False)
-            instance.usuario=request.user
-            instance.autor=request.user.first_name + " " + request.user.last_name
+            instance = formulario.save(commit=False)
+            instance.usuario = request.user
+            instance.autor = request.user.first_name + " " + request.user.last_name
             instance.save()
             for i in imagenNoticia:
-                img = ImagenNoticia(imagen = i,noticia = instance)
+                img = ImagenNoticia(imagen=i, noticia=instance)
                 img.save()
                 # instance.imagenes.add(img)
             # instance.save()
@@ -281,8 +313,7 @@ def escribir(request):
             print(formulario.errors)
             messages.error(
                 request, 'Ha habido un error.', extra_tags='fallo')
-            data={'form': formulario}
-            
+            data = {'form': formulario}
 
     return render(request, "escribir.html", data)
 
@@ -290,9 +321,9 @@ def escribir(request):
 def galeria_api(request):
     noticias = requests.get("http://127.0.0.1:8000/api/noticias/").json()
     # contexto = {"noticias": noticias}
-    page = request.GET.get('page',1)
+    page = request.GET.get('page', 1)
     try:
-        paginator = Paginator(noticias,5)
+        paginator = Paginator(noticias, 5)
         noticias = paginator.page(page)
     except:
         raise Http404
